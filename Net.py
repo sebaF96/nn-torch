4b5942919d6ace6050f81bbf33cf379b2c2a8df2
sebaf96 <sefernandez96@gmail.com>
2020-10-28 01:26:09 -0300

    Train the model, test accuracy and test random images

--- Net.py
+++ Net.py
@@ -1,10 +1,11 @@
 import torch
-import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-
+from matplotlib import pyplot
+import time
 
 """
 torchvision nos provee una coleccion de datos para fines educativos. En la vida real
@@ -44,10 +45,11 @@ class Net(nn.Module):   # Creamos nuestra red como una clase normal de Python qu
         # Metodo que define como la data "va a fluir" por nuestra red
 
         """
-        Una de las ventajas de Pytorch es la libertad para personalizar esta parte.
+        Una de las ventajas de PyTorch es la libertad para personalizar esta parte.
         En este caso pasamos la informacion por todas las layers en el orden que las definimos
-        y para cada uno llamamos a torch.nn.functional.relu (rectified linear unit function element-wise)
-        que sería nuestra Funcion de activacion. (corre en el lado de 'output' de nuestras self.fcs)
+        (no necesariamente tiene q ser asi) y para cada uno llamamos a torch.nn.functional.relu
+        (rectified linear unit function element-wise) que sería nuestra Funcion de activacion,
+        la cual corre en el lado de 'output' de nuestras self.fcs
         """
         data = F.relu(self.fc1(data))
         data = F.relu(self.fc2(data))
@@ -58,6 +60,84 @@ class Net(nn.Module):   # Creamos nuestra red como una clase normal de Python qu
 
 
 
+if __name__ == '__main__':
+    net = Net()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    """
+    parameters() son los parametros "ajustables" de nuestra red. Por ahora todos los pesos y biases.
+    learning rate (lr) puede verse como el tamaño de los pasos que dara nuestra red mientras esta aprendiendo,
+    no debe ser ni muy grande ni muy chico ya que esto haria que se quede atascada. El lr se puede ir modificando
+    mientras avanza el entrenamiento (es comun ir achicandolo a medida que la red va entrenando).
+    Para este ejemplo quedará siempre asi.
+    """
 
-net = Net()
-print(net)
+    # Funcion que itera sobre el trainset y ajusta pesos y biases
+    def train():
+        # Iteraremos 3 veces sobre nuestro trainset completo
+        EPOCHS = 3
+
+        for epoch in range(EPOCHS):
+            for batch in trainset:  # data es cada lote o batch, en nuestro caso lotes de 8 imagenes y 8 labels
+                images, labels = batch
+                net.zero_grad()
+
+                # Pasamos el batch por la red
+                output = net(images.view(-1, 28 * 28))
+
+                # Calculamos el loss
+                loss = F.nll_loss(output, labels)
+                loss.backward()
+
+                # Ajustamos el peso
+                optimizer.step()
+
+            # La red comienza a entrenar, y para cada EPOCH vamos viendo el loss
+            print(loss)
+
+    # Funcion que itera sobre el testset y al final muestra el porcentaje de imagenes que predijo correctamente
+    def evaluate():
+        with torch.no_grad():
+            net.eval()  # No queremos que nuestra red tome en cuenta estos resultados ya que es fase de evaluacion
+
+            corrects = total = 0
+
+            for batch in testset:
+                images, labels = batch
+
+                output = net(images.view(-1, 28 * 28))
+
+                for idx, i in enumerate(output):
+                    if torch.argmax(i) == labels[idx]:
+                        corrects += 1
+                    total += 1
+
+        print(f"Nuestra red predijo correctamente un {round(corrects / total * 100, 2)}% de las imagenes")
+
+    # Funcion que nos muestra 8 imagenes y las predicciones de la red.
+    def slow_evaluation():
+        with torch.no_grad():
+            net.eval()
+
+            # Agarramos el primer batch del lote de prueba
+            for batch in testset:
+                # Iteramos sobre las 8 imagenes
+                for img in batch[0]:
+
+                    # Metemos la imagen a la red y almacenamos la salida
+                    output = net(img.view(-1, 28 * 28))
+
+                    # Mostramos la prediccion de la red
+                    print(f"Creo que esta imagen es un {torch.argmax(output)}, vos que decis?")
+
+                    # Esperamos un ratito y mostramos la imagen
+                    time.sleep(1.5)
+                    pyplot.imshow(img.view(28, 28))
+                    pyplot.show()
+                break
+
+
+
+
+    train()
+    # evaluate()
+    slow_evaluation()
--- experiment.py
+++ experiment.py
@@ -1,5 +1,6 @@
-from Net import trainset
+from Net import trainset, Net
 from matplotlib import pyplot
+import torch
 
 
 def print_dataset():
@@ -20,9 +21,8 @@ def print_dataset():
     pyplot.show()
 
 
-# Que tan balanceado esta nuestro dataset?
-
 def show_balance():
+    # Que tan balanceado esta nuestro dataset?
     total = 0
     counter = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
@@ -37,8 +37,21 @@ def show_balance():
         print(f"El numero {label} aparece {quantity} veces ({str(quantity/total * 100)[:4]} %)")
 
 
+def data_trough_network():
+    net = Net()
+    # print(net)
+
+    # Generamos un tensor aleatorio del mismo tamaño que nuestras imagenes, y se lo damos como input a la red
+    random_image = torch.rand(28, 28).view(-1, 28 * 28)
+
+    # Almacenamos la salida de la red e imprimimos el resultado (predicciones) por ahora sin sentido
+    output = net(random_image)
+    print(output)
+
+
 if __name__ == '__main__':
     #  print_dataset()
     #  show_balance()
+    data_trough_network()
 
     pass
